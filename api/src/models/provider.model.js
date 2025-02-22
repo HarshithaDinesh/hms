8c2ed32345bf09f3235efeb63f888bb33c8da26b
HarshithaDinesh <harshithadinesh97@gmail.com>
2025-02-22 15:36:00 +0530

    Add collection-schemas for users, patients and provider

---
+++ api/src/models/provider.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const providerSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    organisation: { type: String, required: true, trim: true },
+    specialization: { type: String, required: true },
+    patients: [{ type: mongoose.Schema.Types.ObjectId, ref: "Patient" }], // Array of patient IDs
+  },
+  { timestamps: true }
+);
+
+const Provider = mongoose.model("Provider", providerSchema);
+module.exports = Provider;
