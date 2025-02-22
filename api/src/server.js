c8587d5f5b82aee4ca04ce7790468ef14dc8a9c3
HarshithaDinesh <harshithadinesh97@gmail.com>
2025-02-22 14:40:33 +0530

    api-layer setup

---
+++ api/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("API is running..."));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
