bdd4f0b03811375dc4ac00c2ff8c1cfa5343a9c5
HarshithaDinesh <harshithadinesh97@gmail.com>
2025-02-22 16:48:51 +0530

    Parvaiz code - Login & Registration

---
+++ client/src/pages/Registration.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+// or
+// import { FilledInput } from '@mui/material';
+
+function Registration() {
+    
+   const[email, setEmail] =useState('')
+   const[name, setname] =useState('')
+   const[password, setpassword] =useState('')
+   const[age, setage] =useState('')
+   const[gender, setgender] =useState('')
+   const[height, setheight] =useState('')
+   const[weight, setweight] =useState('')
+   
+    async function register(){
+await axios.post("/userApi/Register",{email, name, password, age, gender, height, weight})
+.then((res)=>{
+    console.log(res.data)
+}).catch((err=>{
+    console.log(err)
+}))
+
+    }
+  return (
+    <>
+    <h1>Registration</h1>
+    <div>
+        <input value={email} type='text' placeholder='enter email' onChange={(e)=>{setEmail(e.target.value)}}/>
+    </div>
+    <div>
+        <input type='text' value={name} placeholder='enter name' onChange={(e)=>{setname(e.target.value)}}/>
+    </div>
+    <div>
+        <input type='password' value={password} placeholder='enter password' onChange={(e)=>{setpassword(e.target.value)}}/>
+    </div>
+    <div>
+        <input type='text' value={age} placeholder='enter age' onChange={(e)=>{setage(e.target.value)}}/>
+    </div>
+    <div>
+        <input type='text' value ={gender} placeholder='enter gender' onChange={(e)=>{setgender(e.target.value)}}/>
+    </div>
+    <div>
+        <input type='number' value={height} placeholder='enter height' onChange={(e)=>{setheight(e.target.value)}}/>
+    </div>
+    <div>
+        <input type='number'value={weight} placeholder='enter weight' onChange={(e)=>{setweight(e.target.value)}}/>
+    </div>
+    <button onClick={register}>Register</button>
+    </>
+  )
+}
+
+export default Registration
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
-const Login = () => {
+function Login() {
+  const[email, setEmail] = useState("")
+  const[password, setpassword] = useState("")
+
+  async function loginn(){
+    await axios.post("/userApi/login",{email, password})
+    .then((res)=>{
+      console.log(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+
+  }
+
   return (
-    <div>login</div>
+    <>
+  <div>
+    <div>
+      <input placeholder='enter email' value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+    </div>
+    <div>
+      <input placeholder='enter password' value={password} onChange={(e)=>{setpassword(e.target.value)}}/>
+    </div>
+    <div>
+      <button onClick={loginn}>Login</button>
+    </div>
+  </div>
+    </>
   )
 }
 
